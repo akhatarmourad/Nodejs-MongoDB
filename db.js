2854a5fda59f71761054f3a565726103771ad5cd
akhatarmourad <mourad.akhatar01@gmail.com>
2024-11-11 19:45:29 +0100

    Final Touch

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ import 'dotenv/config';
 
 export const connectDB = async (callback) => {
     try {
-        const client = new MongoClient(`${process.env.MONGODB_URI}`);
+        const client = new MongoClient(`${process.env.MONGODB_ATLAS_URI}`);
         await client.connect();
         console.log('Connected to MongoDB');
 
-        return callback(null, client.db(process.env.MONGODB_DB));
+        return callback(null, client.db("library"));
     }
     catch(error) {
         console.log(error);
